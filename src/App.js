37cc7f7cd3f9d666a0bad2afc2b9c0db68267be1
Charanaraj N <goudacharanraj760@gmail.com>
2023-04-12 22:50:25 +0530

    layout and file struc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <h1>GPT-3</h1>;
-}
+import React from "react";
+
+import {
+  Header,
+  WhatGPT3,
+  Feature,
+  Possibility,
+  Blog,
+  Footer,
+} from "./containers";
+import { Navbar, Brand, CTA } from "./components";
+import "./app.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
